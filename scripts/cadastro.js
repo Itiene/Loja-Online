bedc65fb1f12209d2f6fdf731f64b12adfab5fee
Itiene Guilherme <64323258+Itiene@users.noreply.github.com>
2026-07-14 18:05:32 +0100

    Atualizado

---
+++ scripts/cadastro.js
@@ -0,0 +1,10 @@
+const cadastro=document.querySelector("cadastro")
+const fuser=[...document.querySelector("fuser")]
+const password=[...document.querySelector("password")]
+const confirm_password=[...document.querySelector("confirm_password")]
+const btn_criar=document.querySelector("btn_criar")
+
+
+btn_criar.addEventListener("click",(e)=>{
+    
+})
